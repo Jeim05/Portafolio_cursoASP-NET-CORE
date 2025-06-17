afbf27bf7b17ff4a10f2deb311b0aeac215e1c37
Jeim05 <lopezvegajeimmy@gmail.com>
2025-06-16 20:54:29 -0600

    Agregado de vistas parciales

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,7 +15,42 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var proyectos = ObtenerProyectos().Take(3).ToList();
+        var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+        return View(modelo);
+    }
+
+    private List<Proyecto> ObtenerProyectos()
+    {
+        return new List<Proyecto>() { new Proyecto
+        {
+            Titulo = "Amazon",
+            Descripcion = "E-Commerce realizado en ASP .NET Core",
+            Link = "",
+            ImgURL = "/img/steam.PNG"
+        },
+        new Proyecto
+        {
+            Titulo = "Proyecto 2",
+            Descripcion = "E-Commerce realizado en ASP .NET Core",
+            Link = "",
+            ImgURL = "/img/reddit.PNG"
+        },
+        new Proyecto
+        {
+            Titulo = "Proyecto 3",
+            Descripcion = "Sistema de facturación",
+            Link = "",
+            ImgURL = "/img/nyt.PNG"
+        },
+        new Proyecto
+        {
+            Titulo = "Proyecto 4",
+            Descripcion = "Tienda en linea",
+            Link = "",
+            ImgURL = "/img/nyt.PNG"
+        },
+        };
     }
 
     public IActionResult Privacy()
